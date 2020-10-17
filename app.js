e87ebe98e106f0327dbf62079f5eabd80df28725
Lakshya Bansal <39180629+lakshya-20@users.noreply.github.com>
2020-10-17 17:36:45 +0530

    added mongodb connection and config files

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-const app=express()
-const PORT=process.env.PORT ||5000
+const {mongourl}=require('./config/key');
 
+var connect=mongoose.connect(mongourl);
+connect.then((db) =>{
+  console.log('Connected correctly to mongodb');
+},(err)=>{console.log(err)});
+
+const app=express()
 app.use(express.json())
+
+const PORT=process.env.PORT ||5000
 app.listen(PORT,()=>{
     console.log("Server starting on port no: ",PORT)
 })
