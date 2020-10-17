78ae7ae8304b2522c956608ccb3b6047a39bc592
Lakshya Bansal <39180629+lakshya-20@users.noreply.github.com>
2020-10-17 17:32:03 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app=express()
+const PORT=process.env.PORT ||5000
+
+app.use(express.json())
+app.listen(PORT,()=>{
+    console.log("Server starting on port no: ",PORT)
+})
