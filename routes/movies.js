7cd495514457958c2ed6bf3b6ee8493de5ef4d09
Lakshya Bansal <bansallakshya94128@gmail.com>
2021-07-01 00:12:58 +0530

    Using redis cache for development

--- routes/movies.js
+++ routes/movies.js
@@ -2,37 +2,35 @@ const express = require('express')
 const mongoose = require('mongoose')
 const sanitize = require('mongo-sanitize');
 const requireLogin = require('../util/requireLogin')
-//const redisClient=require('../util/redisClient');
 const logger=require('../util/winstonLogger');
- 
 const {Movies_data} =  require('../models/movie');
-
 const router = express.Router()
-
-
 // @route   Get api/movies
 // @desc    Get list of all movies
 // @access  Public
 router.get('/',async(req,res)=>{    
     try{
-        
-        // redisClient.get("movies",async (err,data)=>{
-            
-        //     if(err){
-        //         logger.error(`${err} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-        //         res.status(500).send("Server Error");
-        //     }
-        //     if(data!=null){                                                
-        //         res.send(data);
-        //     }
-        //     else{                
-        //         const movies= await Movies_data.find();
-        //         redisClient.setex("movies",3600,JSON.stringify(movies));
-        //         res.json(movies);
-        //     }            
-        // })
-        const movies= await Movies_data.find();
-        res.json(movies);
+        if(process.env.NODE_ENV==="development"){
+            const redisClient=require('../util/redisClient');
+            redisClient.get("movies",async (err,data)=>{            
+                if(err){
+                    logger.error(`${err} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+                    res.status(500).send("Server Error");
+                }
+                if(data!=null){                                                
+                    res.send(JSON.parse(data));
+                }
+                else{                
+                    const movies= await Movies_data.find();
+                    redisClient.setex("movies",3600,JSON.stringify(movies));
+                    res.json(movies);
+                }            
+            })
+        }
+        else {
+            const movies= await Movies_data.find();
+            res.json(movies);
+        }
     }catch(err){
         logger.error(`${err} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
         res.status(500).send('Server Error');
@@ -66,7 +64,4 @@ router.get('/:movieId',async (req,res)=>{
         res.status(500).send("Server Error");
     }
 })
-
-
-
 module.exports=router
